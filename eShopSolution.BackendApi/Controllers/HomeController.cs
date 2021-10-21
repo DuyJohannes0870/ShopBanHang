49c505833e5210434ae1a132811c6e8d38469c94
DuyJohannes0870 <duy.hpk0870@sinhvien.hoasen.edu.vn>
2021-10-21 17:51:21 +0700

    test

---
+++ eShopSolution.BackendApi/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//hỗ trợ IntelliSense hoàn tất cho API, như LogWarning và LogError
+using eShopSolution.BackendApi.Models;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
